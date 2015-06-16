ecb6f0115e2d9932f16d004a17bca221918710a9
Dan Bissonnet <dan@danisadesigner.com>
2015-06-16 09:32:31 +0100

    HookerInteface is now a marker
    Classes do not really need to hook themselves most of the time. It's better to mark them as hookable and let
    as bootstrap class choose how to hook them.

--- src/HookableInterface.php
+++ src/HookableInterface.php
@@ -1,12 +1,9 @@
 <?php
 /**
- * Hookeable classes can be passed a Hooker which will register
- * their methods in the WordPress hook system.
+ * Marker interface to indicate the class can be auto-hooked
  *
  * @author Dan Bissonnet <dan@danisadesigner.com>
  */
 namespace DBisso\Hooker;
 
-interface HookableInterface {
-	public function hook( HookerInterface $hooker, $prefix = null );
-}
+interface HookableInterface {}
